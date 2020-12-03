19edcf5b3ad5f3a73e7da7b46460a76c20d24205
Dhruv <cool.dhruv225@gmail.com>
2020-12-03 08:00:16 +0530

    Added sounds on clicking

--- games.js
+++ games.js
@@ -1,20 +1,24 @@
-var randomNumber, randonChosenNumber;
+var randomNumber, randomChosenColor;
 
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
-$(".btn").click((event)=>
-{
+$(".btn").click((event)=> {
     userClickedPattern.push(event.target.id);
     //console.log(userClickedPattern);
+    playSound(event.target.id);
 });
 
 function nextSequence() {
   randomNumber = Math.floor(4 * Math.random());
-  randonChosenNumber = buttonColors[randomNumber]; 
-  gamePattern.push(randomNumber);
-  $("#"+randonChosenNumber).fadeIn(100).fadeOut(100).fadeIn(100);
-  var audio = new Audio("sounds/"+randonChosenNumber+".mp3");
+  randomChosenColor = buttonColors[randomNumber]; 
+  gamePattern.push(randomChosenColor);
+  $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/"+name+".mp3");
   audio.play();
 }
